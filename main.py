67bd398f64525b0f24388fca88d9324f42d66ee7
EtanPtl <130949411+EtanPtl@users.noreply.github.com>
2024-09-18 22:43:18 -0400

    Added 2nd Hidden Layer for improved Accuracy

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ X_train = X_train / 255.
 # Neural Network
 
 def init_params():
-    W1 = np.random.rand(10, 784) - 0.5
-    b1 = np.random.rand(10, 1) - 0.5
-    W2 = np.random.rand(10, 10) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
-    return W1, b1, W2, b2 
+    W1 = np.random.rand(128, 784) - 0.5
+    b1 = np.random.rand(128, 1) - 0.5
+    W2 = np.random.rand(64, 128) - 0.5
+    b2 = np.random.rand(64, 1) - 0.5
+    W3 = np.random.rand(10, 64) - 0.5
+    b3 = np.random.rand(10,1)
+
+    return W1, b1, W2, b2, W3, b3 
 
 def relu(Z):
     return np.maximum(0, Z)
@@ -36,12 +39,14 @@ def softmax(Z):
     return np.exp(Z) / sum(np.exp(Z))
 
 
-def forward_propagation(W1, b1, W2, b2, X):
+def forward_propagation(W1, b1, W2, b2, W3, b3, X):
     Z1 = W1.dot(X) + b1
     A1 = relu(Z1)
     Z2 = W2.dot(A1) + b2
-    A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
+    A2 = relu(Z2)
+    Z3 = W3.dot(A2) + b3
+    A3 = softmax(Z3)
+    return Z1, A1, Z2, A2, Z3, A3
 
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
@@ -51,23 +56,30 @@ def one_hot(Y):
 def derivative_Relu(Z):
     return Z > 0
 
-def back_propagation(Z1, A1, Z2, A2, W2, X, Y):
+def back_propagation(Z1, A1, Z2, A2, Z3, A3, W3, W2, X, Y):
     #m = Y.size
     one_hot_Y = one_hot(Y)
-    dZ2 = A2 - one_hot_Y
+    dZ3 = A3 - one_hot_Y
+    dW3 = 1/m * dZ3.dot(A2.T)
+    db3 = 1/m * np.sum(dZ3)
+
+    dZ2 = W3.T.dot(dZ3) * derivative_Relu(Z2)
     dW2 = 1/m * dZ2.dot(A1.T)
     db2 = 1/m * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2)
+    dZ1 = W2.T.dot(dZ2) * derivative_Relu(Z1)
     dW1 = 1/m * dZ1.dot(X.T)
     db1 = 1/m * np.sum(dZ1)
-    return dW1, db1, dW2, db2
 
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2 , alpha):
+    return dW1, db1, dW2, db2, dW3, db3
+
+def update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
     W1 = W1 - alpha * dW1    
     b1 = b1 - alpha * db1
     W2 = W2 - alpha * dW2
     b2 = b2 - alpha * db2
-    return W1, b1, W2, b2
+    W3 = W3 - alpha * dW3
+    b3 = b3 - alpha * db3
+    return W1, b1, W2, b2, W3, b3
 
 def get_predictions(A2):
     return np.argmax(A2, 0)
@@ -76,29 +88,29 @@ def get_accuracy(predictions, Y):
     return np.sum(predictions == Y) / Y.size
 
 def gradient_descent(X, Y, iterations, alpha):
-    W1, b1, W2, b2 = init_params()
+    W1, b1, W2, b2, W3, b3 = init_params()
     for i in range(iterations+ 1):
-        Z1, A1, Z2, A2 = forward_propagation(W1, b2, W2, b2, X)
-        dW1, db1, dW2, db2 = back_propagation(Z1, A1, Z2, A2, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        Z1, A1, Z2, A2, Z3, A3 = forward_propagation(W1, b1, W2, b2, W3, b3, X)
+        dW1, db1, dW2, db2, dW3, db3 = back_propagation(Z1, A1, Z2, A2, Z3, A3, W3, W2, X, Y)
+        W1, b1, W2, b2, W3, b3 = update_params(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha)
 
-        if i % 50 == 0:
+        if i % 10 == 0:
             print("Iteration: ", i)
-            print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
+            print("Accuracy: ", get_accuracy(get_predictions(A3), Y))
     
-    return W1, b1, W2, b2
+    return W1, b1, W2, b2, W3, b3
 
-W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 1300, 0.1)
+W1, b1, W2, b2, W3, b3 = gradient_descent(X_train, Y_train, 300, 0.1)
 
 
-def make_predictions(X, W1, b1, W2, b2):
-    _, _, _, A2 = forward_propagation(W1, b1, W2, b2, X)
-    predictions = get_predictions(A2)
+def make_predictions(X, W1, b1, W2, b2, W3, b3):
+    _, _, _, _, _, A3 = forward_propagation(W1, b1, W2, b2, W3, b3, X)
+    predictions = get_predictions(A3)
     return predictions
 
-def test_predictions(index, W1, b1, W2, b2):
+def test_predictions(index, W1, b1, W2, b2, W3, b3):
     current_img = X_train[:, index, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2, W3, b3)
     label = Y_train[index]
     print("Prediciton: ", prediction)
     print("Label: ", label)
@@ -108,8 +120,8 @@ def test_predictions(index, W1, b1, W2, b2):
     plt.imshow(current_img, interpolation='nearest')
     plt.show()
 
-test_predictions(0, W1, b1, W2, b2)
-test_predictions(1, W1, b1, W2, b2)
-test_predictions(2, W1, b1, W2, b2)
-test_predictions(3, W1, b1, W2, b2)
-test_predictions(4, W1, b1, W2, b2)
+test_predictions(0, W1, b1, W2, b2, W3, b3)
+test_predictions(1, W1, b1, W2, b2, W3, b3)
+test_predictions(2, W1, b1, W2, b2, W3, b3)
+test_predictions(3, W1, b1, W2, b2, W3, b3)
+test_predictions(4, W1, b1, W2, b2, W3, b3)
